d627d58602e08e0bb97d82400a45b5f6cc5c1d50
ralph <ralphh.fahed@gmail.com>
2024-12-10 19:00:43 +0000

    if else statement

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main ()
+{
+int n;
+
+// Initialize or input a value for 'n'
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+if (n>0 ){
+	printf("is positive\n");
+}else if (n == 0 ){
+	printf ("is zero\n");
+}else{
+	printf("is negative\n");
+}
+return (0); 
+}
