d4c68b32d78cb77cc896fce6e2a9c2c637e1eddc
ralph <ralphh.fahed@gmail.com>
2024-12-10 15:45:16 +0000

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the size of various types on the computer it is compiled and run on.
+ * main - Prints the size of various types on the computand run on.
  *
  * Return: Always 0 (Success)
  */
