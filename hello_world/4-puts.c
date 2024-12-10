5d03ca9f00570807692e02e58bb612d9ebf8903b
ralph <ralphh.fahed@gmail.com>
2024-12-10 15:03:53 +0000

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main(void)
+// Function to print the message
+void print_message(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
+}
+
+// Main function that calls the helper function
+int main(void)
+{
+    print_message();
     return 0;
 }
+
