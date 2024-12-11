3b3811b53ccdbd8abecb7e0571b03d321eaa810f
ralph <ralphh.fahed@gmail.com>
2024-12-11 13:14:09 +0000

    taks 0 ,write a fnc witth write

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
-#include <unistd.h>
+#include <unistd.h> /* Include the correct header for write */
 #include <stdio.h>
-/*ffaddfdsf
- * fcdsffs
- * fdsf
- * */
 
+/**
+ * main - prints "_putchar" followed by a new line
+ * Return: 0 on success, indicating the program executed successfully
+ */
 int main(void)
 {
-write(1, "_putchar\n", 9);
-return(0);
+write(1, "_putchar\n", 9); /*  Use write to print "_putchar" followed by a newline */
+return (0);
 }
