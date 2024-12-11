ed17c9b2d2df335c91553aad2ac9bf824dc0039f
ralph <ralphh.fahed@gmail.com>
2024-12-11 07:56:36 +0000

    reverse alphabet

---
+++ variables_if_else_while/7-print-tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * a to z
+ * a for loop to count a to z bkzin C progrme letter are numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter;
+for (letter = 'z' ; letter >= 'a' ; letter--)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
