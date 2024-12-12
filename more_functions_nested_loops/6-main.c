3937258ef417979104955465366f0b30002effcf
ralph <ralphh.fahed@gmail.com>
2024-12-12 17:58:14 +0000

    task 6

---
+++ more_functions_nested_loops/6-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Check the code for the _islower function.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_line(0);
+    print_line(2);
+    print_line(10);
+    print_line(-4);
+    return (0);
+}
--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
 
+/**
+ * more_numbers - print numbers from 0 to 9
+ *
+ * return : return the numbers
+ */
+void print_line(int n){
+	int i;
+
+	if(n<=0){
+		_putchar('\n');
+	}
+	else
+	{
+	       	for (i=0;i<n;i++)
+		{
+			_putchar('_');
+	
+		}
+		_putchar('\n');
+	}
+
+}
