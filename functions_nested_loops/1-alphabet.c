ebc89fce8ca64a9a9f84bf0fa797130904adcbae
ralph <ralphh.fahed@gmail.com>
2024-12-12 10:39:16 +0000

    task1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,19 +7,17 @@
 void print_alphabet(void)
 {
     char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
-    char buffer[270]; // 26 letters + newline * 10 = 270 characters
+    char buffer[270];
     int index = 0;
 
-    // Create a buffer containing the alphabet 10 times
     for (int i = 0; i < 10; i++)
     {
-        for (int j = 0; j < 27; j++) // 26 letters + newline
+        for (int j = 0; j < 27; j++)
         {
             buffer[index++] = alphabet[j];
         }
     }
 
-    // Print the buffer using _putchar
     for (int k = 0; k < index; k++)
     {
         _putchar(buffer[k]);
