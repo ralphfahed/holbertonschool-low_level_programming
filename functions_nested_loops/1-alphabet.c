cf7affb3a3d317be1ebed494bcd9f8894876e816
ralph <ralphh.fahed@gmail.com>
2024-12-12 11:12:21 +0000

    task1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,12 @@
 #include "main.h"
 
-/**
- * print_alphabet - Prints the alphabet in lowercase followed by a new line
- */
 void print_alphabet(void)
 {
-char alphabet[] = "abcdefghijklmnopqrstuvwxyz"; /* Def string */
-int i;  /* Declare 'i' before the loop */
+    char i;
+    
+    for (i = 'a'; i <= 'z'; i++) {
+        _putchar(i);
+    }
+    _putchar('\n');
+}
 
-    /* Loop through the string and print each character */
-for (i = 0; alphabet[i] != '\0'; i++)
-{
-_putchar(alphabet[i]);  /* Print each character */
-}
-_putchar('\n');  /* Print a newline character */
-}
