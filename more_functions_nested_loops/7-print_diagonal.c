e0feacf005e43aae8b71a395ff35223bcf7142df
ralph <ralphh.fahed@gmail.com>
2024-12-12 20:54:45 +0000

    task 7 + betty

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -3,29 +3,32 @@
 #include <unistd.h>
 
 /**
- * more_numbers - print numbers from 0 to 9
- *i kel ma zedet n byenzal 3al sater
- *j hiye kel ma nezel 3al sater zid space
- * return : return the numbers
+ * print_diagonal - Draws a diagonal line in the terminal
+ * @n: The number of times the '\' character should be printed
+ *
+ * Description: This function prints a diagonal line using the '\'
+ * character. Each line starts with spaces to align the '\' diagonally.
+ * If n is 0 or less, it only prints a newline.
  */
+void print_diagonal(int n)
+{
+int i;
+int j;
 
-void print_diagonal(int n){
-	int i;
-	int j;
-
-	if(n<=0){
-		_putchar('\n');
-	}
-	else
-	{
-	       	for (i=0;i<n;i++)
-		{
-			for (j=1;j<=i;j++){
-				_putchar(' ');
-		}
-			_putchar('\\');
-                        _putchar('\n');
-		}
-	}
-
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < n; i++)
+{
+for (j = 1; j <= i; j++)
+i{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
 }
