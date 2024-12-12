5922f08e0e37181382b5afd12746eb51960b48a6
ralph <ralphh.fahed@gmail.com>
2024-12-12 20:42:04 +0000

    task 6 + betty style

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,20 +7,19 @@
  *
  * return : return the numbers
  */
-void print_line(int n){
-	int i;
-
-	if(n<=0){
-		_putchar('\n');
-	}
-	else
-	{
-	       	for (i=0;i<n;i++)
-		{
-			_putchar('_');
-	
-		}
-		_putchar('\n');
-	}
-
+void print_line(int n)
+{
+int i;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
 }
