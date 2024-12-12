407055eb9b6d782b22675b20759e73688050d186
ralph <ralphh.fahed@gmail.com>
2024-12-12 18:21:25 +0000

    task 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -4,7 +4,8 @@
 
 /**
  * more_numbers - print numbers from 0 to 9
- *
+ *i kel ma zedet n byenzal 3al sater
+ *j hiye kel ma nezel 3al sater zid space
  * return : return the numbers
  */
 
@@ -19,7 +20,7 @@ void print_diagonal(int n){
 	{
 	       	for (i=0;i<n;i++)
 		{
-			for (j=0;j<=i;j++){
+			for (j=1;j<=i;j++){
 				_putchar(' ');
 		}
 			_putchar('\\');
