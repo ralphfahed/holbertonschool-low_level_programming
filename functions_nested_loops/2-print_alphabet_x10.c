dbab2dccaccbe1b87b949571673a003bcdb5fcb4
ralph <ralphh.fahed@gmail.com>
2024-12-12 12:40:07 +0000

    task2 print alphabet 10 times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase followed by a new line
+ * print_alphabet_x10 - Prints the alphabet 10 times in lowercase, each followed by a new line.
+ *
+ * Description: This function prints the alphabet from 'a' to 'z' in lowercase,
+ * 10 times. Each time, the alphabet is printed on a new line.
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void) /* print alphabet 10 times */
 {
 char i;
 char j;
