f65963adafc948dcb46cf9bb29f8015886e0e962
ralph <ralphh.fahed@gmail.com>
2024-12-12 10:41:01 +0000

    task1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,25 +1,27 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase 10 times,
- * followed by a new line.
+ * print_alphabet - Prints the alphabet in lowercase 10 times,
+ *                  followed by a newline.
  */
 void print_alphabet(void)
 {
-    char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
-    char buffer[270];
-    int index = 0;
+    char buffer[270]; /* 26 letters + newline * 10 = 270 characters */
+    int i;            /* Loop counter for 10 iterations */
+    int j;            /* Loop counter for alphabet characters */
+    int index = 0;    /* Index for the buffer */
 
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 27; j++)
-        {
-            buffer[index++] = alphabet[j];
+    /* Build the output in a buffer */
+    for (i = 0; i < 10; i++) {
+        for (j = 'a'; j <= 'z'; j++) {
+            buffer[index++] = j;
         }
+        buffer[index++] = '\n';
     }
 
-    for (int k = 0; k < index; k++)
-    {
-        _putchar(buffer[k]);
+    /* Use _putchar to print the buffer */
+    for (i = 0; i < index; i++) {
+        _putchar(buffer[i]);
     }
 }
+
