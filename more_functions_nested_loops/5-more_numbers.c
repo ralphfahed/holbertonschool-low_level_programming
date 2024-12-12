77968ad3eb12250c34d60ec099cb668e4557a2ea
ralph <ralphh.fahed@gmail.com>
2024-12-12 17:36:21 +0000

    task 5 with betty

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,8 +2,14 @@
 #include "main.h"
 #include <unistd.h>
 
+/**
+ * more_numbers - print numbers from 0 to 9
+ *
+ * 
+ * return : return the numbers
+ */
 
-void more_numbers(void)
+void more_numbers(void) /* print 0 to 14 10times */
 {
 char i;
 char j;
@@ -14,7 +20,7 @@ for (j = 0; j <= 14; j++)
 {
 if (j > 9)
 {
- _putchar('0' + (j / 10));
+_putchar('0' + (j / 10));
 }
 _putchar('0' + (j % 10));
 }
