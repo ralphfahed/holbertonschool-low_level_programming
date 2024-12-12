55facd68846487a02f3b75670e6cc1b1d8aa29b6
ralph <ralphh.fahed@gmail.com>
2024-12-12 17:24:00 +0000

    task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,22 +2,22 @@
 #include "main.h"
 #include <unistd.h>
 
-#include <stdio.h>
-#include "main.h"
 
 void more_numbers(void)
 {
-    char i;
-    char j;
+char i;
+char j;
 
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j <= 14; j++) {
-            if (j > 9) {
-                _putchar('0' + (j / 10));
-            }
-            _putchar('0' + (j % 10));
-        }
-        _putchar('\n');
-    }
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j > 9)
+{
+ _putchar('0' + (j / 10));
 }
-
+_putchar('0' + (j % 10));
+}
+_putchar('\n');
+}
+}
