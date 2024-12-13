65f86da3d1d6dfd11d959ad68ca3abd9f9f3f465
ralph <ralphh.fahed@gmail.com>
2024-12-13 13:16:37 +0000

    task 10 +betty

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,27 +8,21 @@
  */
 void print_triangle(int size)
 {
-    int i, j;
+int row, space;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
 
-    for (i = 1; i <= size; i++)
-    {
-        for (j = 0; j < size - i; j++)
-        {
-            _putchar(' ');
-        }
-
-        for (j = 0; j < i; j++)
-        {
-            _putchar('#');
-        }
-
-        _putchar('\n');
-    }
+for (row = 1; row <= size; row++)
+{
+for (space = 0; space < size - row; space++)
+_putchar(' ');
+for (space = 0; space < row; space++)
+_putchar('#');
+_putchar('\n');
+}
 }
 
