f6fc5c2a001ba88a78620ff4f76258121aba405e
ralph <ralphh.fahed@gmail.com>
2024-12-13 12:11:22 +0000

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,77 +1,60 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_number - Prints an integer
+ * @n: The number to be printed
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
+}
+
+/**
+ * print_to_98 - Prints all numbers from n to 98
+ * @n: Starting number
+ *
+ * Description: This function prints all numbers from n to 98,
+ * regardless of whether n is greater than or less than 98.
+ * Numbers are separated by commas and spaces.
+ */
 void print_to_98(int n)
 {
-    int i;
-
-    if (n < 98)
-    {
-        for (i = n; i <= 98; i++)
-        {
-            if (i != n)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-            if (i < 0)
-            {
-                _putchar('-');
-                if (i < -9)
-                {
-                    _putchar((-(i / 10)) + '0');
-                }
-                _putchar((-(i % 10)) + '0');
-            }
-            else if (i < 10)
-            {
-                _putchar(i + '0');
-            }
-            else
-            {
-                _putchar(i / 10 + '0');
-                _putchar(i % 10 + '0');
-            }
-        }
-    }
-    else if (n == 98)
-    {
-        _putchar('9');
-        _putchar('8');
-    }
-    else
-    {
-        for (i = n; i >= 98; i--)
-        {
-            if (i != n)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-            if (i < 0)
-            {
-                _putchar('-');
-                if (i < -9)
-                {
-                    _putchar((-(i / 10)) + '0');
-                }
-                _putchar((-(i % 10)) + '0');
-            }
-            else if (i < 10)
-            {
-                _putchar(i + '0');
-            }
-            else
-            {
-                if (i >= 100)
-                {
-                    _putchar(i / 100 + '0');
-                    _putchar((i / 10) % 10 + '0');
-                }
-                _putchar(i % 10 + '0');
-            }
-        }
-    }
-    _putchar('\n');
+	if (n < 98)
+	{
+		for (; n <= 98; n++)
+		{
+			print_number(n);
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else if (n == 98)
+	{
+		print_number(n);
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			print_number(n);
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
 }
 
