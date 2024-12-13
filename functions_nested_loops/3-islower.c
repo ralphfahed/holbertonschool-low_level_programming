e6abf8e66102d04d57bd29970ddc740c62278de6
ralph <ralphh.fahed@gmail.com>
2024-12-13 08:16:41 +0000

    task 3 with betty

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include "main.h"
+#include <unistd.h> /* Include the correct header for write */
 
-int _islower(int c) {
-    if (c >= 'a' && c <= 'z') {
-        return 1;
-    } else {
-        return 0;
-    }
+/**
+ *  _islower - check for lower case
+ *  @c: The character to check.
+ * Return: 0 on success, indicating the program executed successfully
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
