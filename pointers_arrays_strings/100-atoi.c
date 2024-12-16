615a6523093fc37e1376851e0d60785c257bebe5
ralph <ralphh.fahed@gmail.com>
2024-12-16 21:29:06 +0000

    task 10 convert a string to int

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The input string.
+ *
+ * Return: The integer value of the string, or 0 if no numbers found.
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+			break;
+	}
+
+	for (; s[i] >= '0' && s[i] <= '9'; i++)
+		result = result * 10 + (s[i] - '0');
+
+	return (result * sign);
+}
