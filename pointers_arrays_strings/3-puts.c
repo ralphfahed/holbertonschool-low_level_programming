70e34b395538c1c769d6b14e0b66bbbae0e83a60
ralph <ralphh.fahed@gmail.com>
2024-12-16 10:10:04 +0000

    task 3 print a string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string followed by a new line.
+ * @str: pointer to the string to be printed.
+ */
+void _puts(char *str)
+{
+printf("%s\n", str);
+}
