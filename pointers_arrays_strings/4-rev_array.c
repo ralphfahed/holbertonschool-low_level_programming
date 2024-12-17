ecc62bf3bf2accf0ddfde7dce5d2b0a84c7f992d
ralph <ralphh.fahed@gmail.com>
2024-12-17 16:08:07 +0000

    tsk 4 reverse a array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_SIZE 1000
+
+/**
+ * reverse_array - Reverses the contents of an integer array.
+ * @a: A pointer to the array of integers.
+ * @n: The number of elements in the array to reverse.
+ *
+ * Description: This function takes an array of integers and its size as input.
+ * It reverses the elements of the array in-place.
+ * 
+ * Return: Nothing.
+ */
+void reverse_array(int *a, int n)
+{
+int temp[MAX_SIZE];
+int i;
+for (i = 0; i < n; i++)
+{
+temp[i] = a[n - 1 - i];
+}
+for (i = 0; i < n; i++)
+{
+a[i] = temp[i];
+}
+}
