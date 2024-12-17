e039d4c760a82778efb82c681238827138927db6
ralph <ralphh.fahed@gmail.com>
2024-12-17 15:26:43 +0000

    task 3 compare two str

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,26 +6,22 @@
  * @s1: The first string.
  * @s2: The second string.
  *   0 if the strings are equal.
- *   -1 if s1 is less than s2.
- *   1 if s1 is greater than s2.
+ *   A negative value if s1 is less than s2.
+ *   A positive value if s1 is greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-	{
-		if (s1[i] < s2[i])
-			return (-15); /* s1 is less than s2 */
-		else if (s1[i] > s2[i])
-			return (15);  /* s1 is greater than s2 */
-	}
-
-	if (s1[i] == '\0' && s2[i] == '\0')
-		return (0);  /* Strings are equal */
-	else if (s1[i] == '\0')
-		return (-15); /* s1 is shorter than s2 */
-	else
-		return (15);  /* s1 is longer than s2 */
+int i;
+for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+{
+if (s1[i] < s2[i])
+{
+return (s1[i] - s2[i]);
 }
-
+else if (s1[i] > s2[i])
+{
+return (s1[i] - s2[i]);
+}
+}
+return (s1[i] - s2[i]);
+}
