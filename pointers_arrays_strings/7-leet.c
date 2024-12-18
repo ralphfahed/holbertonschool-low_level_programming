de4ce4a5041c0eea477ecbb0b6270692130993b8
ralph <ralphh.fahed@gmail.com>
2024-12-18 15:04:11 +0000

    task 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into leet speak.
+ * @s: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *leet(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+		{
+			s[i] = '4';
+		}
+		else if (s[i] == 'e' || s[i] == 'E')
+		{
+			s[i] = '3';
+		}
+		else if (s[i] == 'o' || s[i] == 'O')
+		{
+			s[i] = '0';
+		}
+		else if (s[i] == 't' || s[i] == 'T')
+		{
+			s[i] = '7';
+		}
+		else if (s[i] == 'l' || s[i] == 'L')
+		{
+			s[i] = '1';
+		}
+		i++;
+	}
+
+	return (s);
+}
+
