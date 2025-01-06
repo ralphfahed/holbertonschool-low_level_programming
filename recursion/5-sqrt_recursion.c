5beaf0ccd3d7c1bd90e5ef0a46c823b62ada6b7d
ralph <ralphh.fahed@gmail.com>
2025-01-06 14:50:31 +0000

    task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,17 +9,13 @@
  */
 int _sqrt_recursion_helper(int n, int guess)
 {
-    if (guess * guess == n)
-    {
-        return guess;  /* Found the square root */
-    }
+	if (guess * guess == n)
+		return (guess);	/* Found the square root */
 
-    if (guess * guess > n)
-    {
-        return -1;  /* No natural square root */
-    }
+	if (guess * guess > n)
+		return (-1);	/* No natural square root */
 
-    return _sqrt_recursion_helper(n, guess + 1);  /* Recurse with the next guess */
+	return (_sqrt_recursion_helper(n, guess + 1));
 }
 
 /**
@@ -30,10 +26,8 @@ int _sqrt_recursion_helper(int n, int guess)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-    {
-        return -1;  /* Square root of negative numbers doesn't exist */
-    }
+	if (n < 0)
+		return (-1);	/* Square root of negative numbers doesn't exist */
 
-    return _sqrt_recursion_helper(n, 0);  /* Start recursion with guess = 0 */
+	return (_sqrt_recursion_helper(n, 0));	/* Start recursion with guess = 0 */
 }
