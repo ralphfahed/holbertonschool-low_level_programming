c62c53853d3e3e76c44d2bec1906de7e6721efcc
ralph <ralphh.fahed@gmail.com>
2025-01-06 11:23:21 +0000

    task 2 return a length

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
+
 /**
- * _strlen - Returns the length of a string.
+ * _strlen_recursion - Returns the length of a str using recursion.
  * @s: The string to measure.
  *
  * Return: The length of the string.
  */
 int _strlen_recursion(char *s)
 {
-int length = 0;
-while (*s != '\0')/*Traverse the string until the null terminator*/
-{
-length++;
-s++;/*Move to the next character*/
-}
-return (length);/*Return the length of the string*/
+	if (*s == '\0')  /* Base case: if the string is empty */
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 
