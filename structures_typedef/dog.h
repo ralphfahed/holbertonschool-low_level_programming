dc4c3e0c2a850b3f1ac9059c13285daafa3defd9
ralph <ralphh.fahed@gmail.com>
2025-01-13 09:55:34 +0000

    task3 typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,10 +15,5 @@ char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog
-{
-char *name;
-float age;
-char *owner;
-}dog_t;
+typedef struct dog dog_t;
 #endif /* DOG_H */
