58f5c304031bde6e759584cd1ce40d84e1d5ad2e
ralph <ralphh.fahed@gmail.com>
2025-01-13 09:22:14 +0000

    task 0 struct example

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Struct to store information about a dog
+ * @name: The name of the dog
+ * @age: The age of the dog (in years)
+ * @owner: The name of the dog's owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
