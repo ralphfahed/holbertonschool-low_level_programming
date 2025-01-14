31b1edc08507371ace9eec0076131438ac5b42de
ralph <ralphh.fahed@gmail.com>
2025-01-14 14:26:10 +0000

    task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -7,13 +7,13 @@
  * @cmp: Pointer to the function used to compare values
  *
  * Return: Index of the first element for which cmp does not return 0,
- * or -1 if no match is found or size is <= 0
+ * or -1 if no match is found, array or cmp is NULL, or size is <= 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
