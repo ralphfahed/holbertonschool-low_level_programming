a943c9027ffb59121ff93438b57a317f69901ba5
ralph <ralphh.fahed@gmail.com>
2025-01-17 10:18:32 +0000

    task 3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h> /* For NULL */
+
+/**
+ * get_op_func - Selects the correct function to perform the operation
+ * @s: The operator passed as argument
+ *
+ * Return: Pointer to the function that corresponds to the operator,
+ *         or NULL if no match is found
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s) /* Only this one `if` statement */
+			return (ops[i].f);  /* Return the corresponding function pointer */
+		i++;
+	}
+	return (NULL); /* Return NULL if no match is found */
+}
+
---
+++ function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Performs the requested arithmetic operation
+ * @argc: The number of arguments
+ * @argv: The arguments
+ *
+ * Return: 0 if successful, or 98/99/100 on error
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int (*operation)(int, int);
+
+	if (argc != 4) /* Check for the correct number of arguments */
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]); /* Convert first argument to integer */
+	num2 = atoi(argv[3]); /* Convert second argument to integer */
+
+	operation = get_op_func(argv[2]); /* Get the function for the operator */
+
+	if (operation == NULL) /* Check if operator is valid */
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((*(argv[2]) == '/' || *(argv[2]) == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	printf("%d\n", operation(num1, num2));
+	return (0);
+}
+
