02c6f3df955b40b6ae4a4fd0be3eabda0cf99207
ralph <ralphh.fahed@gmail.com>
2025-01-17 12:38:03 +0000

    task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,49 +4,44 @@
 /**
  * print_all - Prints anything based on the format provided
  * @format: The list of types of arguments passed to the function
+ * 
  * Return: Nothing
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	int i = 0;
-	char *str;
+    va_list args;
+    int i = 0;
+    char *str;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (format && format[i] != '\0')  /* Loop through the format string */
-	{
-		if (i != 0)
-			printf(", ");  /* Print a separator between arguments */
+    while (format && format[i] != '\0')  /* Loop through the format string */
+    {
+        if (i != 0)  /* Print separator if it's not the first argument */
+            printf(", ");
 
-		if (format[i] == 'c')  /* Handle char */
-		{
-			char c = va_arg(args, int);  /* char is promoted to int in va_arg */
-		printf("%c", c);
-		}
-		else if (format[i] == 'i')  /* Handle int */
-		{
-			int num = va_arg(args, int);
-		printf("%d", num);
-		}
-		else if (format[i] == 'f')  /* Handle float */
-		{
-			float f = va_arg(args, double);  /* float is promoted to double in va_arg */
-		printf("%f", f);
-		}
-		else if (format[i] == 's')  /* Handle string */
-		{
-			str = va_arg(args, char *);
-			if (str)
-				printf("%s", str);
-			else
-				printf("(nil)");
-		}
+        if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')  /* Handle types */
+        {
+            if (format[i] == 'c')  /* Handle char */
+                printf("%c", va_arg(args, int));  /* char is promoted to int in va_arg */
+            if (format[i] == 'i')  /* Handle int */
+                printf("%d", va_arg(args, int));
+            if (format[i] == 'f')  /* Handle float */
+                printf("%f", va_arg(args, double));  /* float is promoted to double in va_arg */
+            if (format[i] == 's')  /* Handle string */
+            {
+                str = va_arg(args, char *);
+                if (str)
+                    printf("%s", str);
+                else
+                    printf("(nil)");
+            }
+        }
 
-		i++;  /* Move to the next character in the format string */
-	}
+        i++;  /* Move to the next character in the format string */
+    }
 
-	printf("\n");  /* Print a new line at the end */
-	va_end(args);  /* Clean up va_list */
+    printf("\n");  /* Print a new line at the end */
+    va_end(args);  /* Clean up va_list */
 }
 
