e8b4aa39948953848ef7a83f49ef6ea091f47dbb
ralph <ralphh.fahed@gmail.com>
2025-01-27 14:35:47 +0000

    task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,6 +13,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
     list_t *new_node;
+    unsigned int len = 0;
 
     new_node = malloc(sizeof(list_t));
     if (new_node == NULL)
@@ -21,7 +22,11 @@ list_t *add_node(list_t **head, const char *str)
     if (new_node->str == NULL)
         return (NULL);
 
-    new_node->len = strlen(new_node->str);
+    /* Manually calculate the length of the string */
+    while (str[len] != '\0')
+        len++;
+
+    new_node->len = len;
     new_node->next = *head;
     *head = new_node;
 
