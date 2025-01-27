66dbe023cd24cacd5169a23743a52368a30156ca
ralph <ralphh.fahed@gmail.com>
2025-01-27 12:34:20 +0000

    task 1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,19 +1,21 @@
-root@1ee2294a75b84a1bb6b836762017aa2b-2377118072:/holbertonschool-low_level_programming/singly_linked_lists# cat 0-print_list.c 
-#include <stdio.h>
 #include "lists.h"
+
 /**
- * print_list - prints all the elements of a list_t list.
+ * list_len - returns the number of elements in a linked list_t list.
  * @h: pointer to the head of the list.
  *
- * Return: the number of nodes.
+ * Return: the number of elements in the list.
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
-size_t count = 0;
-while (h != NULL)
-{
-h = h->next;
-count++;
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
-return (count);
-}
+
