3754c74ae2e0568e175dff27440b0cf7a7aae53b
ralph <ralphh.fahed@gmail.com>
2025-01-27 11:06:08 +0000

    task 1

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -20,6 +20,6 @@ typedef struct dlistint_s
 
 /* Function prototype for print_dlistint */
 size_t print_dlistint(const dlistint_t *h);
-
+size_t dlistint_len(const dlistint_t *h);
 #endif /* LISTS_H */
 
