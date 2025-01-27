d0350cbae2e2bb6f886501298f98db9c4a871cc0
ralph <ralphh.fahed@gmail.com>
2025-01-27 14:52:47 +0000

    task

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -20,7 +20,6 @@ list_t *add_node(list_t **head, const char *str)
         return (NULL);
     new_node->str = strdup(str);
     if (new_node->str == NULL)
-	    free(new_node);
         return (NULL);
 
     /* Manually calculate the length of the string */
