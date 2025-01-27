81ef36190ae65f8842c316b7e166acaa83dbaf1f
ralph <ralphh.fahed@gmail.com>
2025-01-27 14:24:06 +0000

    task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,19 +11,14 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *new_node;
+	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
-	{
-		free(new_node);
 		return (NULL);
-	}
-
 	new_node->next = *head;
 	*head = new_node;
 
