717776345c2b91d2f67ffd0ce4f5af34059f24e2
ralph <ralphh.fahed@gmail.com>
2025-01-28 14:23:21 +0000

    task2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,12 +10,11 @@
  */
 size_t _strlen(const char *str)
 {
-    size_t len = 0;
+	size_t len = 0;
 
-    while (str[len])
-        len++;
-
-    return (len);
+	while (str[len])
+		len++;
+	return (len);
 }
 
 /**
@@ -27,33 +26,28 @@ size_t _strlen(const char *str)
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    char *dup_str;
+	list_t *new_node;
+	char *dup_str;
 
-    if (!head || !str)
-        return (NULL);
+	if (!head || !str)
+		return (NULL);
 
-    /* Duplicate the string */
-    dup_str = strdup(str);
-    if (!dup_str)
-        return (NULL);
+	dup_str = strdup(str);
+	if (!dup_str)
+		return (NULL);
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(list_t));
-    if (!new_node)
-    {
-        free(dup_str);
-        return (NULL);
-    }
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+	{
+		free(dup_str);
+		return (NULL);
+	}
 
-    /* Initialize the new node */
-    new_node->str = dup_str;
-    new_node->len = _strlen(dup_str);
-    new_node->next = *head;
+	new_node->str = dup_str;
+	new_node->len = _strlen(dup_str);
+	new_node->next = *head;
+	*head = new_node;
 
-    /* Update the head pointer to the new node */
-    *head = new_node;
-
-    return (new_node);
+	return (new_node);
 }
 
