58c14f4794c779382a1383dd44869d4b81f92978
ralph <ralphh.fahed@gmail.com>
2025-01-29 11:47:38 +0000

    task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,18 +1,20 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * create_new_node - Creates a new node with the provided data.
- * @n: The data to be inserted in the new node.
+ * create_new_node - Creates a new node with the given data
+ * @n: The data to store in the new node
  *
- * Return: The new node, or NULL if it failed.
+ * Return: Pointer to the newly created node,or NULL if memory allocation fail
  */
 dlistint_t *create_new_node(int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-	if (!new_node)
+	if (new_node == NULL)
+	{
 		return (NULL);
+	}
 
 	new_node->n = n;
 	new_node->next = NULL;
@@ -22,28 +24,46 @@ dlistint_t *create_new_node(int n)
 }
 
 /**
- * insert_at_index - Inserts a node at a specific index in the list.
- * @h: Pointer to the head of the list.
- * @temp: The node just before the index.
- * @n: The value of the new node.
+ * get_node_at_index - Returns the node at the given index in the list
+ * @temp: The head of the list to search from
+ * @index: The index of the node to find
  *
- * Return: The address of the new node, or NULL if it failed.
+ * Return: Pointer to the node at the given index, or NULL
  */
-dlistint_t *insert_at_index(dlistint_t **h, dlistint_t *temp, int n)
+dlistint_t *get_node_at_index(dlistint_t *temp, int index)
 {
+	int current_index = 0;
 
-dlistint_t *new_node;
-(void)h;
-new_node = create_new_node(n);
+	while (temp != NULL && current_index < index)
+	{
+		temp = temp->next;
+		current_index++;
+	}
 
-	if (!new_node)
+	if (temp == NULL || current_index != index)
+	{
 		return (NULL);
+	}
 
+	return (temp);
+}
+
+/**
+ * insert_node_after - Inserts a new node after a given node
+ * @temp: The node to insert after
+ * @new_node: The new node to insert
+ *
+ * Return: Pointer to the new node that was inserted
+ */
+dlistint_t *insert_node_after(dlistint_t *temp, dlistint_t *new_node)
+{
 	new_node->next = temp->next;
 	new_node->prev = temp;
 
 	if (temp->next)
+	{
 		temp->next->prev = new_node;
+	}
 
 	temp->next = new_node;
 
@@ -51,35 +71,38 @@ new_node = create_new_node(n);
 }
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a specific position.
- * @h: Pointer to the head of the list.
- * @idx: The index where the new node should be inserted.
- * @n: The data for the new node.
+ * insert_at_index - Inserts a new node at a given index in the list
+ * @h: Double pointer to the head of the list
+ * @temp: The current node to start the search from
+ * @n: The data to insert at the specified index
  *
- * Return: The address of the new node, or NULL if it failed.
+ * Return: Pointer to the new node, or NULL if insertion fails
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_at_index(dlistint_t **h, dlistint_t *temp, int n)
 {
-	dlistint_t *temp = *h;
-	unsigned int i = 0;
+	dlistint_t *new_node;
 
-	/* If the index is 0, use the function to insert at the beginning */
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	/* Traverse the list to find the node at the given index */
-	while (temp)
+	/* Return NULL if the list is empty or index is out of range */
+	if (temp == NULL)
 	{
-		if (i == idx - 1)
-			break;
-		temp = temp->next;
-		i++;
+		return (NULL);
 	}
 
-	/* Check if we reached the end or if idx is out of bounds */
+	/* Get the node at the specified index */
+	temp = get_node_at_index(temp, n);
 	if (temp == NULL)
-		return (add_dnodeint_end(h, n));
+	{
+		return (NULL);
+	}
+
+	/* Create a new node */
+	new_node = create_new_node(n);
+	if (!new_node)
+	{
+		return (NULL);
+	}
 
 	/* Insert the new node at the desired position */
-return (insert_at_index(h, temp, n));
+	return (insert_node_after(temp, new_node));
 }
+
