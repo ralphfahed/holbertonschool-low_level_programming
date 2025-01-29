7f6f465c140835c7d87f13a2df73b254ef52906d
ralph <ralphh.fahed@gmail.com>
2025-01-29 09:33:20 +0000

    task 3

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -26,6 +26,12 @@ size_t dlistint_len(const dlistint_t *h);
 
 /* Function prototype to add a node at the beginning of a dlistint_t list */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif /* LISTS_H */
 
