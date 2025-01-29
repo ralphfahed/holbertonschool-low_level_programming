513f5594776d0be21661a641e6bc4cb6b6cffbb9
ralph <ralphh.fahed@gmail.com>
2025-01-29 09:42:59 +0000

    task 4 + commands

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -9,11 +9,11 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
 
-	while (head != NULL)
+	while (head != NULL) /* is head fiya value */
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		temp = head->next; /* temp hiye nextnode */
+		free(head); /* free the first node =head*/
+		head = temp; /* next node btekhod ma7al awalnode */
 	}
 }
 
