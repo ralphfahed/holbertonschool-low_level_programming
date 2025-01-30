1ead276c1e7a800aabc577669e5bd8cb69143a7a
ralph <ralphh.fahed@gmail.com>
2025-01-30 10:38:18 +0000

    task 3

--- file_io/main.h
+++ file_io/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
-#include <sys/types.h>
-#define BUF_SIZE 1024
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#define BUF_SIZE 1024
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
